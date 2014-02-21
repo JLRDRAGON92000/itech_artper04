09167fb3939878d566d837fab31eb655a4d01c27
JLRDRAGON92000 <jlrdragon@gmail.com>
2014-02-21 18:30:54 -0500

    still testing...

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Untitled Document</title>
+		<title>MASSIVE awesomeness!</title>
 	</head>
-
+	
 	<body>
-		
+    	<span style="color:#8CE2FF;">as i type this it should <span style="color:#AA2020;">show up</span> in the right hand column. lol</span>
 	</body>
 </html>
