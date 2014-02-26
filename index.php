17a67eb3046c760507b6fe5f4dd2852af4435622
JLRDRAGON92000 <jlrdragon@gmail.com>
2014-02-25 19:12:44 -0500

    continued Web design project

--- index.php
+++ index.php
@@ -3,9 +3,48 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>MASSIVE awesomeness!</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
 	</head>
-	
 	<body>
-    	<span style="color:#8CE2FF;">as i type this it should <span style="color:#AA2020;">show up</span> in the right hand column. lol</span>
+		<div id="wrapper">
+			<header id="top">
+				<h1>B@Y51D3 B33T</h1>
+				<nav id="mainnav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="sightseeing.php">Sightseeing</a></li>
+						<li><a href="#">Eating Out</a></li>
+						<li><a href="#">What's On</a></li>
+						<li><a href="#">Where to Stay</a></li>
+					</ul>
+				</nav>
+			</header>
+			<div id="hero">
+				<article>
+				<h2>Be WHere IT's aT</h2>
+				<p>San Francisco is one of the most exciting and vibrant cities on the planet. Bayside Beat is here to keep you informed of the best places to see, where to eat, what to do, and where to lay down your weary head after an action-packed day—or night—on the town.q</p>
+				</article>
+				<img src="images/golden_gate.jpg" />
+			</div>
+			<article id="main">
+				<h2>Riding the Cable Cars</h2>
+				<p>No visit to San Francisco is complete without a ride on the iconic cable cars that climb up the vertiginous hills of the city. Of the twenty-three lines established between 1873 and 1890, three remain: two routes from downtown near Union Square to <a href="http://www.fishermanswharf.org/">Fisherman's Wharf</a>, and a third route along California Street.</p>
+				
+				<p>The cable cars rely on cables running constantly beneath the road’s surface. The driver—or gripman—uses a lever to grip the cable to pull the car and its passengers up the hill. The gripman requires not only great strength, but also great skill. He needs to know where to release the cable to coast over crossing cables and points. The conductor works in close cooperation with the gripman, operating the brake at the rear of the car to prevent it from running out of control on the downward slopes.</p>
+				<figure><img src="images/cable_car1.jpg" /></figure>
+				<figcaption>The cable car terminus near Union Square</figcaption>
+				<p>Although the cable cars are now mainly a tourist attraction, they’re still used by local commuters to get to and from work. The California Street line is particularly popular among commuters on weekdays.</p>
+			</article>
+			<aside id="sidebar">
+				<h2>Cable Car Tips</h2>
+				
+				<p>A single ride on a cable car costs $6. If you plan to travel around the city, it’s often cheaper to buy a <a href="http://www.sfmta.com/cms/mfares/passports.htm">Muni Passport</a>, which gives you unlimited rides on San Francisco’s extensive public transport system, including the cable cars (but not the <a href="http://www.bart.gov/">BART subway</a> system). Even a single-day passport ($14) will save you money if you make a return trip, and stop off to visit Chinatown one way.</p>
+				
+				<p>There are often long lines at the cable car terminus, particularly on the Powell-Mason and Powell-Hyde routes. If you don’t want to wait, try walking a few stops along the route. The conductor usually leaves a small number of places to pick up passengers on the way. The California Street route is generally less crowded (but not as spectacular).</p>
+			</aside>
+			<footer>
+				<p>&copy; Copyright 2013 Bayside Beat</p>
+			</footer>
+		</div>
 	</body>
 </html>
